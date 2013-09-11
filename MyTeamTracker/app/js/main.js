d359f73cbdcd78ec3e55973a290af6b74069f0b7
Rick Wicker <rickjr82@gmail.com>
2013-09-11 12:38:04 -0400

    missed jquery update

--- MyTeamTracker/app/js/main.js
+++ MyTeamTracker/app/js/main.js
@@ -1,7 +1,7 @@
 ﻿require.config({
     paths: {
         angular: '/Scripts/angular',
-        jQuery: '/Scripts/jquery-1.8.2.min',
+        jQuery: '/Scripts/jquery-1.9.1.min',
         underscore: '/Scripts/underscore.min',
         uibootstrap: 'https://raw.github.com/angular-ui/bootstrap/gh-pages/ui-bootstrap-tpls-0.5.0',
         smarttable: 'https://raw.github.com/lorenzofox3/Smart-Table/master/Smart-Table.min'
