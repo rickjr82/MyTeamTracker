cb43098c504f030022f8d1558c4a12964fe73136
Rick Wicker <rickjr82@gmail.com>
2013-09-11 13:08:46 -0400

    typo

--- MyTeamTracker/app/js/main.js
+++ MyTeamTracker/app/js/main.js
@@ -3,7 +3,7 @@
         angular: '/Scripts/angular',
         jQuery: '/Scripts/jquery-1.9.1.min',
         underscore: '/Scripts/underscore.min',
-        uibootstrap: 'Scripts/ui-bootstrap-tpls-0.6.0',
+        uibootstrap: '/Scripts/ui-bootstrap-tpls-0.6.0.min',
         smarttable: '/app/js/lib/Smart-Table.min',
     },
     baseUrl: '/app/js',
