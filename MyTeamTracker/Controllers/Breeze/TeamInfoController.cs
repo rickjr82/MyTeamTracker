4fe2ad92496ed16cadcc9f07a8c33068bf795072
Rick Wicker <rickjr82@gmail.com>
2013-09-18 14:10:22 -0400

    missing files

---
+++ MyTeamTracker/Controllers/Breeze/TeamInfoController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using MyTeamTracker.Models;
+using Breeze.WebApi;
+using Breeze.WebApi.EF;
+using Newtonsoft.Json.Linq;
+namespace MyTeamTracker.Controllers
+{
+    [BreezeController]
+    public class TeamInfoController : ApiController
+    {
+        static DateTime _lastRefresh = DateTime.Now; // will first clear db at Now + "RefreshRate" 
+        // static DateTime lastRefresh = DateTime.MinValue; // will clear when server starts
+
+        readonly EFContextProvider<MyTeamTrackerContext> _contextProvider =
+            new EFContextProvider<MyTeamTrackerContext>();
+
+
+        // ~/breeze/todos/Metadata 
+        [HttpGet]
+        public string Metadata()
+        {
+            return _contextProvider.Metadata();
+        }
+
+        // ~/breeze/todos/Todos
+        // ~/breeze/todos/Todos?$filter=IsArchived eq false&$orderby=CreatedAt 
+        [HttpGet]
+        public IQueryable<Team> Teams()
+        {
+            return _contextProvider.Context.Teams;
+        }
+
+        // ~/breeze/todos/SaveChanges
+        [HttpPost]
+        public SaveResult SaveChanges(JObject saveBundle)
+        {
+            return _contextProvider.SaveChanges(saveBundle);
+        }
+
+    }
+}
